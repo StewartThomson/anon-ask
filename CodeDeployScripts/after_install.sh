cc66e00ec27a5dbfc0e1d58f453ff64c19955831
stewart <stewartthomson3@gmail.com>
2019-10-22 16:41:03 -0400

    fixing env getting

--- CodeDeployScripts/after_install.sh
+++ CodeDeployScripts/after_install.sh
@@ -2,6 +2,4 @@
 
 cd /var/www && npm install
 
-envContent=$(aws ssm get-parameters --region us-east-2 --names anon-ask-env --output text --query Parameters[0].Value)
-
-echo $envContent > /var/www/.env
+aws ssm get-parameters --region us-east-2 --names anon-ask-env --output text --query Parameters[0].Value > .env
