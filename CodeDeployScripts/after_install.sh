14b08b959f7fbcbb65cf01107fddb10e6475a816
stewart <stewartthomson3@gmail.com>
2019-10-22 15:53:11 -0400

    - added region for ssm

--- CodeDeployScripts/after_install.sh
+++ CodeDeployScripts/after_install.sh
@@ -2,6 +2,6 @@
 
 cd /var/www && npm install
 
-envContent=$(aws ssm get-parameters --names anon-ask-env --query Parameters[0].Value)
+envContent=$(aws ssm get-parameters --region us-east-2 --names anon-ask-env --query Parameters[0].Value)
 
 echo $envContent > .env
