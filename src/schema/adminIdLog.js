0cf1b3f9be97653d3875012cc2a27ec454f565e6
stewart <stewartthomson3@gmail.com>
2019-11-03 16:56:00 -0500

    - Changed admin schema to just be "user" that has admin / ban flag. much better

--- src/schema/adminIdLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const adminIdLogSchema = new Schema({
-  workspace_id: String,
-  admin_ids: [String]
-});
-
-module.exports = mongoose.model("AdminIdLog", adminIdLogSchema);
