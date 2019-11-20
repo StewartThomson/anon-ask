14a5fd97bb867609f5c6d3fa20da6dcef5ba44a7
onimisi <onimisiukanah@hotmail.com>
2019-11-20 14:13:31 -0500

    removed duplicate confirmuser function

--- src/features/slack_features.js
+++ src/features/slack_features.js
@@ -15,17 +15,6 @@ async function ConfirmUser(userId) {
   return user.user_id;
 }
 
-async function ConfirmUser(userId) {
-  let user = {};
-  console.log("User ID in function --> "+userId);
-  const [foundUser] = await User.find({ user_id: userId });
-  if (foundUser) {
-    user = foundUser;
-  }
-  // console.log(user.user_id)
-  return user.user_id;
-}
-
 module.exports = function(controller) {
   controller.on('message', async (bot, message) => {
     if (!message.thread_ts) {
